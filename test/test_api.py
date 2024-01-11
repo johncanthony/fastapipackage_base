885c159d95898d16d296a10e59cf7ea98411f13b
johncanthony <john.claud.anthony@gmail.com>
2024-01-11 05:54:09 -0600

    Added test for other job function

--- test/test_api.py
+++ test/test_api.py
@@ -8,3 +8,9 @@ def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_get_jobs():
+    response = client.get("/jobs")
+    assert response.status_code == 200
+    assert response.json() == {"jobs": ["job1", "job2", "job3"]}
